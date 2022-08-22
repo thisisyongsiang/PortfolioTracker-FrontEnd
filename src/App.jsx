9a1dcb784e63a2bb6b7422b1a65df6e74749c3d2
thisisyongsiang <ongys1992@gmail.com>
2022-08-22 15:11:52 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import './App.css';
+
+class App extends React.Component{
+  
+  render(){
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default App;
