493a9da55f2daf2b15e83e7671db40568b636eb4
thisisyongsiang <ongys1992@gmail.com>
2022-08-26 02:54:48 +0800

    Added user list to check

--- src/App.jsx
+++ src/App.jsx
@@ -7,22 +7,65 @@ import { withAuth0,useAuth0 } from '@auth0/auth0-react';
 import LogoutButton from "./auth0/logoutButton";
 import Login from "./auth0/login";
 import Loading from "./auth0/loading";
+import { UserPage } from "./auth0/user";
+import { addUser,getAllUsers } from "./users/users";
+import { Users } from "./users/reactUsers";
 class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={users:[]};
+  }
   
+  onLoggedIn(user){
+    let userObj={
+      lastName:user.family_name,
+      firstName:user.given_name,
+      emailAddress:user.email,
+    }
+    // addUser(userObj);
+  }
+  getUsers(){
+    getAllUsers().then(
+      (users)=>{
+          this.setState({users:users});
+      }
+  );
+  }
+  componentDidMount(){
+    this.getUsers();
+  }
   render(){
     const {user, isAuthenticated,isLoading } = this.props.auth0;
+    if(isAuthenticated&&!isLoading){this.onLoggedIn(user)};
     return (
       <React.Fragment>
-        {(!isAuthenticated &&!isLoading) &&<Login />}
+        {<Login />}
         <div className="container">
           <div className="row">
-            <div className="col-xs-12">
-              <h2>hello</h2>
-              {(!isAuthenticated &&!isLoading)&& <LoginButton />}
-              {(isAuthenticated &&!isLoading)&& <LogoutButton />}
-              {isLoading &&<Loading />}
-            </div>
+              <nav className="navbar">
+                <a href="#" className="navbar-brand">APP NAME</a>
+                <ul className="navbar-nav me-auto">
+                  <li className="nav-item">
+                    <a href="#" className="nav-link">User Profile</a>
+                  </li>
+                </ul>
+              </nav>
           </div>
+              <hr />
+              {(isAuthenticated &&!isLoading)&&
+            <div className="row">
+              <div className="col-xs-12">
+              <UserPage />
+              <LogoutButton />
+              {isLoading &&<Loading />}
+              </div>
+              <hr />
+              <Users users={this.state.users}/>
+            </div>
+  }
+                
+
+
         </div>
         </React.Fragment>
     );
