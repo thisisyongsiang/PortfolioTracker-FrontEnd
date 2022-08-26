80e85953667fa7ff5e67e79406f03007708ae32a
Xiong Zhi <limxiongzhi@gmail.com>
2022-08-26 09:59:48 +0800

    init frame

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,33 @@ import { withAuth0,useAuth0 } from '@auth0/auth0-react';
 import LogoutButton from "./auth0/logoutButton";
 import Login from "./auth0/login";
 import Loading from "./auth0/loading";
+import { BrowserRouter, Route } from "react-router-dom";  // import router for 2 page directory
+import Header from "./Components/Header"; // import header object
+import Banner from "./Components/Banner";
+import AssetTable from "./Components/AssetTable";
+import Homepage from "./Pages/Homepage"; // import homepage object
+import Assetpage from "./Pages/Assetpage";
+import PortfolioCharts from "./Components/PortfolioCharts";
+
 class App extends React.Component{
   
   render(){
     const {user, isAuthenticated,isLoading } = this.props.auth0;
+
     return (
+      <BrowserRouter>
       <React.Fragment>
         {(!isAuthenticated &&!isLoading) &&<Login />}
+
+        <div>
+          < Header />
+          < Banner />
+          < PortfolioCharts />
+          < AssetTable />
+          {/* < Route path = "/" component={Homepage} /> 
+          < Route path = "/asset/:id" component={Assetpage} /> */}
+        </div>
+
         <div className="container">
           <div className="row">
             <div className="col-xs-12">
@@ -24,7 +44,9 @@ class App extends React.Component{
             </div>
           </div>
         </div>
+
         </React.Fragment>
+        </BrowserRouter>
     );
   }
 }
---
+++ src/Components/PortfolioCharts.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PortfolioCharts = () => {
+  return (
+    <div>PortfolioCharts</div>
+  )
+}
+
+export default PortfolioCharts
