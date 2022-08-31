65ea5737a738980f0fcbc1a5205672c19600926c
Salomone <darsalomone@gmail.com>
2022-09-01 03:22:27 +0800

    updated profile pic and pic positioning

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,31 +2,19 @@ import {
   Typography,
 } from "@mui/material";
 import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+
 
 const Header = () => {
-
+  const {user} = useAuth0();
   return (
-    <nav className="navbar-brand h1" style={{backgroundColor: "#484A4F"}}>
-      <div className="container row">
-        <div id="navbar" style={{color: "#FFFFFF"}} className="col align-self-center justify-content-start h5">
-          NavBar
-        </div>
-        <div className="col justify-content-end"></div>
-        <div className="col p-1 align-self-center justify-content-end">
-          <img
-            id="profilepicture"
-            src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-profiles/avatar-1.webp"
-            alt="Generic placeholder image"
-            className="img-fluid"
-            style={{width: "60px", borderRadius: "10px"}}
-          />
-        </div>
-      </div>
- 
-      <Typography
+    <nav className="navbar-brand h1" style={{"backgroundColor": "#484A4F"}}>
+      <div className="container-fluid row">
+        <div id="navbar" style={{"color": "#FFFFFF"}} className="col align-self-center justify-content-start h5">
+          FinDash
+          <Typography
         variant="subtitle2"
-        ml={1.5}
-        mt={-1}
+        mt={0}
         style={{
           color: "darkgrey",
           textTransform: "capitalize",
@@ -35,6 +23,18 @@ const Header = () => {
       >
         Your one-stop, value-added portfolio tracker
       </Typography>
+        </div>
+        <div className="col p-2 m-n2 align-self-center">
+          <img
+            src= {user.picture} //"https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-profiles/avatar-1.webp"
+            alt="Generic placeholder image"
+            style={{"width": "60px", "borderRadius": "10px", "float": "right"}}
+          />
+        </div>
+        
+      </div>
+ 
+      
     </nav>
   );
 };
