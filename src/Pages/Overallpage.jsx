a4ab8676ff5a8253718c77de0263697905f92877
Salomone <darsalomone@gmail.com>
2022-09-05 02:21:30 +0800

    updated net return

--- src/Pages/Overallpage.jsx
+++ src/Pages/Overallpage.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Container } from "@mui/system";
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
 import { LineChart } from "../charts/LineChart";
 import AssetTable from "../Components/AssetTable";
 import PortfolioCharts from "../Components/PortfolioCharts";
@@ -9,6 +11,41 @@ import {
 } from "../users/userApi.js";
 import { numberWithCommas } from "../util/util";
 import CardWidget from "../Components/CardWidget";
+=======
+=======
+>>>>>>> Stashed changes
+import { getUserOverallPortfolioValue ,getUserPortfolioHistoricalValue,getUserPortfolios} from "../users/userApi.js";
+import { numberWithCommas } from "../util/util";
+import CardWidget from "../Components/CardWidget";
+import AssetTable from "../Components/AssetTable";
+import { LineChart } from "../charts/LineChart";
+import { computeOverallPortfolioNetReturn } from "../util/financeComputations";
+
+export const Overallpage=(user)=>{
+    const userData=user.user;
+    const [overallPfValue,setOverallPfValue]=useState(0);
+    const [portfolios,setPortfolios]=useState([]);
+    const [portfoliosHistory,setportfoliosHistory]=useState([]);
+    const lineChartContainer=useRef(null);
+    const [lineChartWidth,setLineChartWidth]=useState(1000);
+    useEffect(()=>{
+      if(lineChartContainer.current){
+        setLineChartWidth(lineChartContainer.current.offsetWidth);
+      }
+      if (userData){
+        getUserPortfolios(userData.emailAddress).then(d=>{
+          setPortfolios(d);
+          getUserPortfolioHistoricalValue(userData.emailAddress,d[0]['portfolio'],
+          new Date(new Date().setFullYear(new Date().getFullYear()-1)),
+          new Date()
+          ).then(pfHist=>{
+            setportfoliosHistory(pfHist);
+          });
+        });
+        getUserOverallPortfolioValue(userData.emailAddress).then(d=>{
+          setOverallPfValue(d);
+        });
+>>>>>>> Stashed changes
 
 export const Overallpage = (user) => {
   const userData = user.user;
@@ -25,6 +62,10 @@ export const Overallpage = (user) => {
     }
   }, [userData]);
 
+    let netReturn = computeOverallPortfolioNetReturn(portfolios, overallPfValue)
+
+    let netReturn = computeOverallPortfolioNetReturn(portfolios, overallPfValue)
+
   return (
     <React.Fragment>
       <Container id="container">
@@ -38,7 +79,7 @@ export const Overallpage = (user) => {
           <div className="position-absolute top-0 end-0">
             <div className="d-flex h-100 p-1">
               <CardWidget type="annReturn" />
-              <CardWidget type="netReturn" />
+              <CardWidget type="netReturn" value={netReturn.toFixed(1)}/>
               <CardWidget type="unrealisedPnL" />
               <CardWidget type="volatility" />
             </div>
